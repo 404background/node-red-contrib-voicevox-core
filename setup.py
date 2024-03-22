85cc56187b88dd64b5f04b2d3d18d589236dfdd0
404background <404background@gmail.com>
2024-03-22 16:59:48 +0900

    Pythonの仮想環境作成、実行

--- setup.py
+++ setup.py
@@ -2,8 +2,28 @@ import os
 import subprocess
 import shutil
 import requests
+import json
 
 absdir = os.path.dirname(os.path.abspath(__file__))
+
+subprocess.run(['python', '-m', 'venv', 'pyenv'])
+
+if os.name == 'nt':
+    subprocess.run([f'{absdir}/pyenv/Scripts/python.exe', '-m', 'pip', 'install', '--upgrade', 'pip'])
+    path = {
+        'NODE_PYENV_PYTHON': f'{absdir}/pyenv/Scripts/python.exe',
+        'NODE_PYENV_PIP': f'{absdir}/pyenv/Scripts/pip.exe'
+    }
+else:
+    subprocess.run([f'{absdir}/pyenv/bin/python', '-m', 'pip', 'install', '--upgrade', 'pip'])
+    path = {
+        'NODE_PYENV_PYTHON': f'{absdir}/pyenv/bin/python',
+        'NODE_PYENV_PIP': f'{absdir}/pyenv/bin/pip'
+    }
+
+with open(f'{absdir}/path.json', 'w') as f:
+    json.dump(path, f, indent=2)
+
 
 
 #pip install
@@ -11,8 +31,8 @@ absdir = os.path.dirname(os.path.abspath(__file__))
 ## for Windows
 if os.name == 'nt':
     package = 'https://github.com/VOICEVOX/voicevox_core/releases/download/0.15.0/voicevox_core-0.15.0+cpu-cp38-abi3-win_amd64.whl'
-    subprocess.run(f'pip install requests')
-    subprocess.run(f'pip install {package}')
+    subprocess.run([f'{absdir}/pyenv/Scripts/pip.exe', 'install', 'requests'])
+    subprocess.run([f'{absdir}/pyenv/Scripts/pip.exe', 'install', f'{package}'])
 
 
 # Execute downloader
@@ -26,4 +46,4 @@ if os.name == 'nt':
         f.write(urlData)
     subprocess.run(filename)
     os.remove(filename)
-    new_path = shutil.copy(f'{absdir}/voicevox_core/onnxruntime.dll', f'{absdir}/onnxruntime.dll')
+    new_path = shutil.copy(f'{absdir}/voicevox_core/onnxruntime.dll', f'{absdir}/pyenv/Lib/site-packages/voicevox_core/onnxruntime.dll')
--- voicevox.js
+++ voicevox.js
@@ -1,21 +1,26 @@
 module.exports = function(RED) {
     function Voicevox(config) {
-        RED.nodes.createNode(this,config);
-        let node = this;
+        RED.nodes.createNode(this,config)
+        let node = this
         node.on('input', function(msg) {
-            const exec = require('child_process').exec;
-            let path = require('path');
+            const exec = require('child_process').exec
+            const fs = require('fs')
+            const path = require('path')
+
             const path_voicevox = path.join(__dirname, 'voicevox.py')
-            const command = 'python ' + path_voicevox + ' ' + msg.payload + ' ' + Number(config.id);
-            const path_voice = __dirname + '/voice/' + msg.payload + '.wav';
+            const path_voice = path.join(__dirname, 'voice', msg.payload + '.wav')
+            const jsonPath = path.join(__dirname, 'path.json')
+            const json = fs.readFileSync(jsonPath)
+            const pythonPath = JSON.parse(json).NODE_PYENV_PYTHON
+            const command = pythonPath + ' ' + path_voicevox + ' ' + msg.payload + ' ' + Number(config.id)
 
             exec(command, (err, stdout, stderr) => {
-                if (err) { console.log(err); }
-                console.log(stdout);
-            });
-            msg.payload = path_voice;
-            node.send(msg);
-        });
+                if (err) { console.log(err) }
+                console.log(stdout)
+            })
+            msg.payload = path_voice
+            node.send(msg)
+        })
     }
-    RED.nodes.registerType("voicevox",Voicevox);
+    RED.nodes.registerType("voicevox",Voicevox)
 }
